dbf8b96ff50363e24457f20ef500dbf602e2768b
arch <arch@local>
2023-05-08 20:35:03 +0200

    add svd

--- funscript_copilot/motion_analyser.py
+++ funscript_copilot/motion_analyser.py
@@ -31,6 +31,28 @@ class MotionAnalyser:
             while not self.should_exit:
                 await asyncio.sleep(1)
 
+    @staticmethod
+    def scale(signal: list, lower: float = 0, upper: float = 99) -> list:
+        """ Scale an signal (list of float or int) between given lower and upper value
+
+        Args:
+            signal (list): list with float or int signal values to scale
+            lower (float): lower scale value
+            upper (float): upper scale value
+
+        Returns:
+            list: list with scaled signal
+        """
+        if len(signal) == 0:
+            return signal
+
+        if len(signal) == 1:
+            return [lower]
+
+        signal_min = min(signal)
+        signal_max = max(signal)
+        return [(float(upper) - float(lower)) * (x - signal_min) / (signal_max - signal_min) + float(lower) for x in signal]
+
 
     def get_low_rank_adoption(self, frame) -> np.ndarray:
         frame = cv2.resize(frame, None, fx=0.1, fy=0.1)
@@ -49,6 +71,7 @@ class MotionAnalyser:
         prev_frame = None
         y_batch = []
         prediction_pca = [[] for _ in range(self.n_components)]
+        prediction_svd = [[] for _ in range(self.n_components)]
         while self.video.isOpened() and not self.stop:
             frame_number += 1
             self.logger.debug(f"Process frame {frame_number}")
@@ -68,14 +91,21 @@ class MotionAnalyser:
 
             if len(y_batch) >= self.batch_size:
                 self.ipca.partial_fit(y_batch)
-                batch_prediction = np.transpose(np.array(self.ipca.transform(y_batch)))
+                batch_prediction_pca = np.transpose(np.array(self.ipca.transform(y_batch)))
+                _, _, batch_prediction_svd = np.linalg.svd(np.transpose(np.array(y_batch)), full_matrices=False)
+                batch_prediction_svd = np.transpose(np.array(batch_prediction_svd[:self.n_components,:]))
                 y_batch = []
                 for i in range(self.n_components):
-                    prediction_pca[i].extend(batch_prediction[i])
+                    prediction_pca[i].extend(batch_prediction_pca[i])
+                    prediction_svd[i].extend(batch_prediction_svd[:,i])
+
+        self.video.release()
 
         if self.n_components == 2:
-            result = np.array(prediction_pca[0]) + np.array(prediction_pca[1])
-            plt.plot(result)
+            result_pca = np.array(prediction_pca[0]) + np.array(prediction_pca[1])
+            result_svd = np.array(prediction_svd[0]) + np.array(prediction_svd[1])
+            plt.plot(MotionAnalyser.scale(result_pca, -100, 100))
+            plt.plot(MotionAnalyser.scale(result_svd, -100, 100))
         else:
             for i in range(self.n_components):
                 plt.plot(prediction_pca[i])
